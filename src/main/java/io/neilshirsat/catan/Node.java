a6fd2eec4bdf5c41ae7f9636544034ec41d798d7
Neila <74432190+neilshirsat@users.noreply.github.com>
2022-04-05 13:41:07 -0500

    delete unneeded gradle directories

--- src/main/java/io/neilshirsat/catan/Node.java
+++ src/main/java/io/neilshirsat/catan/Node.java
@@ -1,24 +1,4 @@
 package io.neilshirsat.catan;
 
-import java.util.List;
-
 public interface Node {
-
-    int getNE();
-
-    int getE();
-
-    int getSE();
-
-    int getNW();
-
-    int getW();
-
-    int getSW();
-
-    boolean isBorderNode();
-
-    boolean hasRobber();
-
-    List<Node> spiralTraversal();
 }
